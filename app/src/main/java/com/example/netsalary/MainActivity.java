29a723672bde09515e820a863f2516efed00b0f2
andre.machado <4ndre.machado@gmail.com>
2020-08-14 16:43:11 -0300

    It is switching the screen when clicking in the buttons

--- app/src/main/java/com/example/netsalary/MainActivity.java
+++ app/src/main/java/com/example/netsalary/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.netsalary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    Button btnCalculate = (Button)findViewById(R.id.btnCalculate);
+
+    btnCalculate.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            //when user click on Calculate Button, go to next screen
+            //parameters = context and results
+            //Intent intent = new Intent(getApplicationContext(), ResultsActivity.class); //option 1
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class); //option 2
+
+            startActivity(intent);
+        }
+    });
+
+
     }
 }
--- app/src/main/java/com/example/netsalary/ResultActivity.java
+++ app/src/main/java/com/example/netsalary/ResultActivity.java
@@ -2,7 +2,10 @@ package com.example.netsalary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Button btnBackToHome = (Button)findViewById(R.id.btnBackToHome);
+
+        btnBackToHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            //click button to go back to first screen (home)
+            //ResultsActivity.this = current activity //(first parameter could be getApplicationContext())
+            Intent intent = new Intent(ResultActivity.this, MainActivity.class);
+
+            startActivity(intent);
+
+            }
+        });
+
     }
 }
